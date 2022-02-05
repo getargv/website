c64acc044102af28c757bd94afcbbe734bb59419
Camden Narzt <c.narzt@me.com>
2022-02-05 16:22:38 -0700

    add uglify and attempt to address touch dropdown

--- assets/ts/dropdown.ts
+++ assets/ts/dropdown.ts
@@ -11,13 +11,13 @@ function fade(el: HTMLElement, keyframes: Keyframe[], completionHandler: Complet
 }
 
 function fadeIn(el: HTMLElement) {
+    if (el.style.getPropertyValue('opacity') == '1') return;
     fade(el, [{ opacity: '0' }, { opacity: '1' }]);
 }
 
 function fadeOut(el: HTMLElement) {
-    fade(el, [{ opacity: '1' }, { opacity: '0' }], function(this: Animation, ev: AnimationPlaybackEvent) {
-        el.style.setProperty('display', 'none');
-    });
+    if (el.style.getPropertyValue('opacity') == '0') return;
+    fade(el, [{ opacity: '1' }, { opacity: '0' }], () => el.style.setProperty('display', 'none'));
 }
 
 function createDropdown(el: Element) {
@@ -40,21 +40,43 @@ addEventListener('DOMContentLoaded', () => {
     const nav = document.getElementById('nav');
     for (const el of nav?.getElementsByClassName('dropdown')!) {
         const dropdown = createDropdown(el);
-
-        const fadeHandler = () => {
+        const parent = el.parentElement;
+        const fadeOutHandler = () => {
             window.clearTimeout(fadeTimeout);
             fadeTimeout = window.setTimeout(() => { fadeOut(dropdown) }, 350);
-        }
-
-        dropdown.addEventListener('mouseleave', fadeHandler);
-        dropdown.addEventListener('mouseenter', () => {
-            window.clearTimeout(fadeTimeout);
-        });
-        el.parentElement?.addEventListener('mouseenter', () => {
+        };
+        const fadeInHandler = () => {
             window.clearTimeout(fadeTimeout);
             fadeIn(dropdown);
+        };
+        const hoverHandler = () => window.clearTimeout(fadeTimeout);
+
+        dropdown.addEventListener('mouseleave', fadeOutHandler);
+        dropdown.addEventListener('mouseenter', hoverHandler);
+        parent?.addEventListener('mouseenter', fadeInHandler);
+        parent?.addEventListener('mouseleave', fadeOutHandler);
+        window.addEventListener('keypress', ev => {
+            if (ev.key == 'Escape') {
+                ev.stopPropagation();
+                ev.preventDefault();
+                window.clearTimeout(fadeTimeout);
+                fadeOut(dropdown);
+            }
         });
-        el.parentElement?.addEventListener('mouseleave', fadeHandler);
+
+        // above probably doesn't work on touch devices, need to handle clicks/scroll too
+        if (document.body.classList.contains('is-touch')) {
+            parent?.addEventListener('click', fadeInHandler);
+            window.addEventListener('click', (ev: MouseEvent) => {
+                if (parent?.contains(ev.target as Node) || dropdown.contains(ev.target as Node)) {
+                    ev.stopPropagation();
+                    ev.preventDefault();
+                    window.clearTimeout(fadeTimeout);
+                    fadeOut(dropdown);
+                }
+            });
+            window.addEventListener('scroll', fadeOutHandler);
+        }
         document.body.appendChild(dropdown);
     }
 });
