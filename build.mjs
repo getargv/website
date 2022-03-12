0c8c031ca726edb663b1d66ea2b32e1926b4fe9a
Camden Narzt <c.narzt@me.com>
2022-03-11 20:44:53 -0700

    update speed with newest measurement from hyperfine
    i suspect that changes to the OS are what caused the slowdown, but i’m not sure how to test that theory atm
    
    building older commits doesn’t speed things up, neither does using -O3 when compiling, and the algorithm hasn’t changed

--- build.mjs
+++ build.mjs
@@ -8,9 +8,10 @@ const sidebar_for = (_,name) => readFileSync(`${name}-sidebar.html`, 'utf8');
 const body = readFileSync(body_file, 'utf8').replaceAll(/<([^ -]+)-sidebar \/>/g,sidebar_for).trim();
 const footer = readFileSync("footer.html", 'utf8').trim();
 const stats = readFileSync("stats.html", 'utf8').trim();
+const speed = readFileSync("speed.html", 'utf8').trim();
 const projects = readFileSync("projects.html", 'utf8').trim();
 const homelink = readFileSync("homelink.html", 'utf8').trim();
-const header = readFileSync("header.html", 'utf8').trim().replace(/<getargv-homelink \/>/,homelink);;
+const header = readFileSync("header.html", 'utf8').trim().replace(/<getargv-homelink \/>/,homelink);
 let template = readFileSync("template.html", 'utf8').trim();
 
 if (body_file != "index-body.html") {
@@ -21,6 +22,7 @@ template = template.replace(/<getargv-header \/>/,header);
 template = template.replace(/<getargv-footer \/>/,footer);
 template = template.replace(/<getargv-main \/>/,body);
 template = template.replace(/<getargv-stats \/>/,stats);
+template = template.replace(/<getargv-speed \/>/,speed);
 template = template.replace(/<getargv-projects \/>/,projects);
 template = template.replace(/<getargv-homelink \/>/,homelink);
 
