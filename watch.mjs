c267887383eeb305c124f1026a6e3fdc728026e2
Camden Narzt <c.narzt@me.com>
2023-03-13 22:42:00 -0600

    simplify watch/start logic

--- watch.mjs
+++ watch.mjs
@@ -9,11 +9,11 @@ const execute=(command,index) => {
             executing[index-1] = false;
             if (error) r(stderr);
             else a(stdout);
-        }));
+        })).catch(console.error);
     }
 };
 
-execute("git ls-files -- *.html",1)
+execute("git ls-files -- src/*.html",1)
     .then(stdout => stdout.split("\n").filter(s => s.length > 0))
     .then(files => {
         const watcher = chokidar.watch(files);
@@ -22,11 +22,11 @@ execute("git ls-files -- *.html",1)
         watcher
             .on('add', path => {
                 log(`File path ${path} has been added`);
-                execute("npm run html --ignore-scripts",1);
+                execute("echo 'html\ndeploy' | xargs -I{} npm run {} --ignore-scripts",1);
             })
             .on('change', path => {
                 log(`File path ${path} has been changed`);
-                execute("npm run html --ignore-scripts",1);
+                execute("echo 'html\ndeploy' | xargs -I{} npm run {} --ignore-scripts",1);
             })
             .on('unlink', path => log(`File ${path} has been removed`))
             .on('error', error => log(`Watcher error: ${error}`))
@@ -45,11 +45,34 @@ execute("git ls-files -- assets/ts/*.ts",2)
         watcher
             .on('add', file => {
                 log(`File path ${file} has been added`);
-                execute("npm run js --ignore-scripts",2);
+                execute("echo 'prejs\njs\ndeploy' | xargs -I{} npm run {} --ignore-scripts",2);
             })
             .on('change', file => {
                 log(`File path ${file} has been changed`);
-                execute("npm run js --ignore-scripts",2);
+                execute("echo 'prejs\njs\ndeploy' | xargs -I{} npm run {} --ignore-scripts",2);
+            })
+            .on('unlink', path => log(`File ${path} has been removed`))
+            .on('error', error => log(`Watcher error: ${error}`))
+            .on('ready', _ => log('Initial scan complete. Ready for changes'))
+        ;
+
+        process.on('SIGTERM', _ => watcher.close().then(_ => log('closed')));
+    });
+
+execute("git ls-files -- assets/sass/*.scss",3)
+    .then(stdout => stdout.split("\n").filter(s => s.length > 0))
+    .then(files => {
+        const watcher = chokidar.watch(files);
+        const log = console.log.bind(console);
+
+        watcher
+            .on('add', file => {
+                log(`File path ${file} has been added`);
+                execute("echo 'css\ndeploy' | xargs -I{} npm run {} --ignore-scripts",3);
+            })
+            .on('change', file => {
+                log(`File path ${file} has been changed`);
+                execute("echo 'css\ndeploy' | xargs -I{} npm run {} --ignore-scripts",3);
             })
             .on('unlink', path => log(`File ${path} has been removed`))
             .on('error', error => log(`Watcher error: ${error}`))
